1372454057143e7daf0ca8c7434f50ecf8201343
Aditya Yadav <166515021+aditya0yadav@users.noreply.github.com>
2025-02-11 20:30:08 +0530

    Update config.py

--- Project-1/config.py
+++ Project-1/config.py
@@ -1,2 +1,2 @@
-GEMINI_API_KEY = "process.env.YOUR_ENV_VARIABLE"
+GEMINI_API_KEY = "process.env.gemini"
 URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
